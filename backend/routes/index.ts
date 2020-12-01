0ebdb3d090324c7f22aebf3c8a6dc088c80ef63a
ShoheiKamiya <49185403+ShoheiKamiya@users.noreply.github.com>
2020-12-01 22:15:35 +0900

    #5 API実装 (#6)
    * add routing
    
    * #7 separate dbconfig
    
    * WIP use ESmodule
    
    * use ES Modules
    
    * add .env.sample
    
    * move dotenv import
    
    * add query

---
+++ backend/routes/index.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+import express from 'express';
+import { Pool } from 'pg';
+import mountains from './mountains';
+import { dbconfig } from '../dbconfig';
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+
+// declare namespace NodeJS {
+//   interface ProcessEnv {
+//     readonly NODE_ENV: 'development' | 'production' | 'test';
+//   }
+// }
+
+const pool = new Pool(dbconfig['development'])
+
+// App
+const app = express();
+app.get('/', async (req: any, res: any) => {
+  const response = await pool.query('SELECT * from users;');
+  res.json(response.rows)
+});
+app.get('/compare', (req: any, res: any) => {
+  res.json(req.query)
+})
+app.use('/mountains', mountains);
+
+app.listen(PORT, HOST);
+console.log(`
+====================================
+  Running on http://${HOST}:${PORT}
+====================================
+`);
