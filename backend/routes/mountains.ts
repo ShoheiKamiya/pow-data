64ce5d9575009c3da440a488343722c892b8ab2d
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-12-01 22:14:11 +0900

    add query

--- backend/routes/mountains.ts
+++ backend/routes/mountains.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import { Pool } from 'pg';
+import { dbconfig } from '../dbconfig';
+
 
 const router = express.Router();
-router.get('/', (req: any, res: any) => {
-  res.json({index: 'スキー場一覧'});
+const pool = new Pool(dbconfig['development'])
+router.get('/', async (req: any, res: any) => {
+  const response = await pool.query('SELECT * from mountains;');
+  res.json(response.rows)
 })
 
-router.get('/:mountainId', (req: any, res: any) => {
-  const params = req.params
-  res.json(params)
+router.get('/:mountainId', async (req: any, res: any) => {
+  const mountainId = req.params.mountainId
+  const response = await pool.query(`SELECT * from mountains where id='${mountainId}'`)
+  res.json(response.rows)
 })
 
 export default router;
